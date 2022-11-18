d5b74e05a85bee41040ea728f80f2d40d3690f99
Tomislav Celic <celer.tomislav@gmail.com>
2022-11-18 01:02:57 +0100

    commiting second homework

---
+++ DZ2/Homework2Celic/Description.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVShow
+{
+    public class Description
+    {
+        private string episodeName;
+        private TimeSpan duration;
+        private uint episodeNumber;
+
+        public Description(uint episodeNumber, TimeSpan duration, string episodeName)
+        {
+            this.episodeNumber = episodeNumber;
+            this.duration = duration;
+            this.episodeName = episodeName;
+        }
+
+        public string EpisodeName
+        {
+            get { return episodeName; }
+        }
+
+        public TimeSpan Duration
+        {
+            get { return duration; }
+        }
+
+        public uint EpisodeNumber
+        {
+            get { return episodeNumber; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.episodeNumber},{this.duration},{this.episodeName}";
+        }
+    }
+}
---
+++ DZ2/Homework2Celic/Episode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVShow
+{
+    public class Episode
+    {
+        private int viewers;
+        private double scoreSum;
+        private double maxScore;
+        private Description description;
+
+        public Episode(int viewers, double scoreSum, double maxScore, Description description)
+        {
+            this.description = description;
+            this.viewers = viewers;
+            this.scoreSum = scoreSum;
+            this.maxScore = maxScore;
+        }
+
+        public void AddView(double score)
+        {
+            this.viewers++;
+            if (this.maxScore < score) this.maxScore = score;
+            scoreSum += score;
+        }
+
+        public double GetAverageScore()
+        {
+            return this.scoreSum / this.viewers;
+        }
+
+        public int GetViewerCount()
+        { 
+            return this.viewers; 
+        }
+
+        public double GetMaxScore()
+        {
+            return maxScore;
+        }
+
+        public static bool operator >(Episode leftHandSide, Episode rightHandSide)
+        {
+            return leftHandSide.GetAverageScore() > rightHandSide.GetAverageScore();
+        }
+
+        public static bool operator <(Episode leftHandSide, Episode rightHandSide)
+        {
+            return leftHandSide.GetAverageScore() < rightHandSide.GetAverageScore();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.viewers},{this.GetAverageScore()},{this.GetMaxScore()},{this.description.ToString()}";
+        }
+    }
+}
